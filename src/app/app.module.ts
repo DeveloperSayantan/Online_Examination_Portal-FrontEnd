ca3a9bda3d47ff6b7dfbecec71d2b7b53ac78aaa
Sohaib Aktar Gazi <aktar19sohaib@gmail.com>
2023-11-22 22:52:27 +0530

    Adding blank Compoments

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { LoginComponent } from './Components/login/login.component';
+import { SignUpComponent } from './Components/sign-up/sign-up.component';
+import { DashboardComponent } from './Components/dashboard/dashboard.component';
+import { IndexPageComponent } from './Components/index-page/index-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    SignUpComponent,
+    DashboardComponent,
+    IndexPageComponent
   ],
   imports: [
     BrowserModule,

3b3b152ff8037befb5710446c954f51f56a0dc00
Sohaib Aktar Gazi <aktar19sohaib@gmail.com>
2023-11-26 22:32:35 +0530

    signup login initial complete work done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import{ LoginComponent } from './Components/login/login.component'
+import{ SignUpComponent } from './Components/sign-up/sign-up.component'
+import { DashboardComponent } from './Components/dashboard/dashboard.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignUpComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  // Add more routes as needed
+  { path: '', redirectTo: '/login', pathMatch: 'full' }, // Redirect to signup by default
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
